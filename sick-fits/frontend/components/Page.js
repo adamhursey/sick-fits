d29e4af1bf34c5ea6ea9e3cda64d858f68168aab
Adam Hursey <adamhursey24@gmail.com>
2021-02-15 16:14:16 -0700

    introduced header and nav

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
+import Header from './Header';
 
 export default function Page({ children }) {
   return (
     <>
+      <Header />
       <h1>I am the page comp</h1>
       {children}
     </>
