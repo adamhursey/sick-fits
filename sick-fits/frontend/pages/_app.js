347d782b2babae430c9ba2084dc38f51b355da32
Adam Hursey <adamhursey24@gmail.com>
2021-02-16 07:42:42 -0700

    Added serverstylesheets for css on reload

--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,7 +1,17 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import PropTypes from 'prop-types';
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+
+// TODO swap with our own
+import 'nprogress/nprogress.css';
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
