44e2b66eff96e0765d2cb59830c4c4218a3cace1
Adam Hursey <adamhursey24@gmail.com>
2022-09-01 09:36:37 -0400

    product query now works too

--- sick-fits/frontend/pages/products/index.js
+++ sick-fits/frontend/pages/products/index.js
@@ -4,11 +4,12 @@ import Products from '../../components/Products';
 
 export default function ProductsPage() {
   const { query } = useRouter();
+  const page = parseInt(query.page);
   return (
     <div>
-      <Pagination page={Number(query.page) || 1} />
-      <Products />
-      <Pagination page={Number(query.page) || 1} />
+      <Pagination page={page || 1} />
+      <Products page={page || 1} />
+      <Pagination page={page || 1} />
     </div>
   );
 }
