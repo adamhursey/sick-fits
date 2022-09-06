6e98e85a64a2d9cc55ebfb3662b89eea2609c266
Adam Hursey <adamhursey24@gmail.com>
2022-09-06 10:13:09 -0400

    sign in works

--- sick-fits/frontend/pages/signin.js
+++ sick-fits/frontend/pages/signin.js
@@ -1,7 +1,9 @@
+import SignIn from '../components/SignIn';
+
 export default function SignInPage() {
   return (
-    <>
-      <p>Sign on in!</p>
-    </>
+    <div>
+      <SignIn />
+    </div>
   );
 }
